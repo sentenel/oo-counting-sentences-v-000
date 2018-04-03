91b243005bb813e1b20099a36f34e9e62995cd07
Seth Wax <sethlwax@gmail.com>
2018-04-03 16:13:53 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    self[-1] == '.'
+    self.end_with?('.')
   end
 
   def question?
-    self[-1] == '?'
+    self.end_with?('?')
   end
 
   def exclamation?
-    self[-1] == '!'
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.gsub(/[^.?!]/, '').length
   end
 end
